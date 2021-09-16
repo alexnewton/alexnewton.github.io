165741bf29997789ba1d2efae4508b3d24930c5d
Alex Newton <alexnewton42@gmail.com>
2021-09-16 14:45:47 -0400

    Update pendo.js

--- pendo.js
+++ pendo.js
@@ -17,7 +17,8 @@
                     name: 'my name',
                     emailid: 'example_01@pendo.io',
                     role: 'Admin',
-                    return_url: 'htpps//www.google.com'
+                    return_url: 'htpps//www.google.com',
+                    segmentTest: myInt
 
                     // You can add any additional visitor level key-values here,
                     // as long as it's not one of the above reserved names.
